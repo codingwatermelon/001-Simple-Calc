eaffc63e73e49f56fe97c5666b3af8fc854d09cd
Jack Torres <JTORR-LAPOSX@Jacks-MacBook-Pro.local>
2017-12-20 15:42:35 -0800

    Initial Commit

---
+++ site/calc.js
@@ -0,0 +1,24 @@
+var field = document.getElementById('result_field');
+
+/* Returns array of all elements with name 'display' */
+numbers = document.getElementsByClassName('display');
+
+for (int i = 0; i < numbers.length; i ++) {
+  numbers[i].setAttribute('onclick', 'post(this)');
+};
+
+function erase() {
+  field.value = '';
+};
+
+/* Creates */
+function post(me) {
+  field.value += me.innerHTML;
+};
+
+/* */
+function submit() {
+  var input = field.value;
+  //eval function insecure?
+  result_field.value = eval(input);
+};
